816cf2467655506787010ac313ec07bd00721ee8
Alexey Shishlyannikov <alexeyshishlyannikov@Alexeys-MacBook-Pro.local>
2019-03-10 20:17:24 -0700

    Added migration

--- server/Migrations/20190311031404_Added data annotations to inventory models.Designer.cs
+++ server/Migrations/20190311031404_Added data annotations to inventory models.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Inspectify.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Inspectify.Migrations
 {
     [DbContext(typeof(InspectifyDbContext))]
-    partial class InspectifyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190311031404_Added data annotations to inventory models")]
+    partial class Addeddataannotationstoinventorymodels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
